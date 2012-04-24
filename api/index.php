384a953229a16cc3334a3bb7aab27ee481896bcb
Jhonny Lidfors <Jhonny@jhocy.com>
2012-04-24 07:22:27 +0200

    just a small fix

--- api/index.php
+++ api/index.php
@@ -8,19 +8,13 @@ Slim_Route::setDefaultConditions(array(
     'id' => '[0-9]+'
 ));
 
-require 'Routes/User.php';
-require 'Routes/Auth.php';
-
-// Autoloader - This might actually turned out to be slower with
-// all the validation in place so we probaly not use it.
-/*$env = $app->environment('PATH_INFO');
-$paramSize = explode('/', $env['PATH_INFO']);
-$param = sizeof($paramSize) > 2 ? substr($env['PATH_INFO'], 0, strrpos($env['PATH_INFO'], '/')) : $env['PATH_INFO'];
-$route = 'Routes' . $param . '.php';
+$env = $app->environment('PATH_INFO');
+$param = explode('/', $env['PATH_INFO']);
+$route = 'Routes/' . $param[1] . '.php';
 
 if(file_exists($route)) {
 	require_once $route;
-}*/
+}
 
 $app->run();
 ?>
